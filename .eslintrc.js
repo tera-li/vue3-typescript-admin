3a65fa4121f6854028a4615951e2f2c5651143d0
ljl <776745393@qq.com>
2021-04-12 21:53:58 +0800

    :bug:fix(一些bug): 一些bug

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,8 @@ module.exports = {
   },
   extends: [
     'plugin:vue/vue3-strongly-recommended',
-    'plugin:prettier/recommended',
-    '@vue/typescript/recommended'
+    '@vue/typescript/recommended',
+    '@vue/standard'
   ],
   parserOptions: {
     ecmaVersion: 2020
