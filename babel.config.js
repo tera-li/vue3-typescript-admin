3f5f8e13d8e5f529d6cb12907d12f1cc57f9f11f
ljl <776745393@qq.com>
2021-10-08 22:12:34 +0800

    :bug:fix(docs(babel): dynamic): ok

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,8 @@
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ],
+  presets: ['@vue/cli-plugin-babel/preset'],
   env: {
     development: {
-      // babel-plugin-dynamic-import-node plugin only does one thing by converting all import() to require().
-      // This plugin can significantly increase the speed of hot updates, when you have a large number of pages.
-      // https://panjiachen.github.io/vue-element-admin-site/guide/advanced/lazy-loading.html
+      // 按需加载
       plugins: [
         'dynamic-import-node'
         // [
